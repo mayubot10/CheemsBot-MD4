9a8778eeee35e8f025392300ac328beea156dc67
mayubot10 <112180094+mayubot10@users.noreply.github.com>
2022-08-27 11:42:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['94764020947'] //ur owner number
+global.ownername = "M_piyuᵐᵃn" //ur owner name
+global.ytname = "https://www.youtube.com/channel/UC18cTg_ihEHuCLpWWtTCWgg " //piyu_beA
+global.socialm = "https://chat.whatsapp.com/DvgNtrv6KVKIRXJGjJo136" //MY official gruop
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94764020947']
+global.ownertag = '94764020947' //ur tag number
+global.botname = 'piyu Bot MD V4' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UC18cTg_ihEHuCLpWWtTCWgg" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UC18cTg_ihEHuCLpWWtTCWgg" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/DvgNtrv6KVKIRXJGjJo136' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "piyu Bot MD V4 " //ur sticker watermark packname
+global.author = "piyu Bot MD V4" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94764020947'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
